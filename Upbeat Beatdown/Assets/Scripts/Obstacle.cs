0594d7ebc34597ceea8a384581ae42408f9d2859
Wade <wadeg@hashbanggames.com>
2018-09-08 16:37:45 -0700

    runner concept start

---
+++ Upbeat Beatdown/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// increases in size for now
+public class Obstacle : MonoBehaviour {
+
+	// change color?
+	private void OnIncrement()
+	{
+
+	}
+
+	private void OnBeat()
+	{
+		transform.localScale = new Vector3(transform.localScale.x * 3f, transform.localScale.y, transform.localScale.z);
+	}
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			OnBeat();
+		}
+	}
+}
---
+++ Upbeat Beatdown/Assets/Scripts/PlayerInputs.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputs : MonoBehaviour {
+	public KeyCode MoveLeft;
+	public KeyCode MoveRight;
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(MoveLeft))
+		{
+			transform.position = new Vector3(transform.position.x -2, transform.position.y, transform.position.z);
+		}
+		if (Input.GetKeyDown(MoveRight))
+		{
+			transform.position = new Vector3(transform.position.x + 2, transform.position.y, transform.position.z);
+		}
+	}
+}
